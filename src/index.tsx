9e7a15de514d6e9b484a5e39b897dcf80165d061
Slobodan Komazec <skomazec@xorantech.com>
2023-02-06 20:53:00 +0100

    handling untrusted code

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ const App = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
   const ref = useRef<any>();
+  const iframe = useRef<any>();
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -23,6 +24,8 @@ const App = () => {
   const onClick = async () => {
     if (!ref.current) return;
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -34,8 +37,29 @@ const App = () => {
       },
     });
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) =>{
+            try{
+            eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+              console.error(err);
+             }
+          },false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -46,7 +70,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 };
