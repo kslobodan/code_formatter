c687fb83527068f4283afbba85954afb683167f9
Slobodan Komazec <skomazec@xorantech.com>
2023-02-16 21:35:23 +0100

    using redux

---
+++ src/components/cell-list.tsx
@@ -0,0 +1,18 @@
+import { useTypdSelector } from "../hooks/use-typed-selector";
+import CellListItem from "./cell-list-item";
+
+const CellList: React.FC = () => {
+  const cells = useTypdSelector(({ cells: { order, data } }) => {
+    return order.map((id) => {
+      return data[id];
+    });
+  });
+
+  const renderedCells = cells.map((cell) => (
+    <CellListItem key={cell.id} cell={cell} />
+  ));
+
+  return <div>{renderedCells}</div>;
+};
+
+export default CellList;
